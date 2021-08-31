caccf4eaaaa539d45945b1120e564ce70b99918f
Jannik Lorenz <janniklorenz1994@gmail.com>
2021-08-31 19:42:03 +0200

    refactor: removes dead code

--- lib/core.ts
+++ lib/core.ts
@@ -1,15 +1,5 @@
-import { PermutationOptions } from "./contracts";
-import { InvalidMaxLengthError } from "./errors";
 import { permutate } from "./utils";
 
-export function safePermutate<T>(input: T[], options?: PermutationOptions) : T[][] {
-    const opts: PermutationOptions = {
-        maxLength: options?.maxLength ?? Infinity,
-    }
-    
-    if(opts.maxLength === 0) {
-        throw new InvalidMaxLengthError()
-    }
-
+export function safePermutate<T>(input: T[]) : T[][] {
     return permutate(input)
 }
--- test/core.spec.ts
+++ test/core.spec.ts
@@ -1,6 +1,4 @@
-import { PermutationOptions } from "../lib/contracts"
 import { safePermutate } from "../lib/core"
-import { InvalidMaxLengthError } from "../lib/errors"
 
 describe("Core", () => {
     it("should return the given input if input length is 1", () => {
@@ -17,25 +15,5 @@ describe("Core", () => {
         const result = safePermutate(input)
 
         expect(result).toEqual([[1, 2], [2, 1]])
-    })
-
-    it("should throw InvalidMaxLengthError if given maxLength is 0", () => {
-        const options: PermutationOptions = {
-            maxLength: 0
-        }
-        const input = [1, 2]
-
-        expect(() => safePermutate(input, options)).toThrowError(InvalidMaxLengthError)
-    })
-
-    it.skip("should return the permutation limited by given maxLength", () => {
-        const options: PermutationOptions = {
-            maxLength: 1
-        }
-        const input = [1, 2]
-
-        const result = safePermutate(input, options)
-
-        expect(result).toHaveLength(1)
     })
 })
