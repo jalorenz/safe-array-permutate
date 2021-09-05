8e3eafdc7322993528300f7d884ba41afddd564b
Jannik Lorenz <janniklorenz1994@gmail.com>
2021-09-05 19:42:27 +0200

    test(core): optimized input

--- test/core.spec.ts
+++ test/core.spec.ts
@@ -1,12 +1,12 @@
-import { safePermutate } from '../lib/core';
-import * as utils from '../lib/utils';
+import { safePermutate } from "../lib";
+import * as utils from "../lib/utils";
 
-jest.mock('../lib/utils', () => ({
-  permutate: jest.fn(),
+jest.mock("../lib/utils", () => ({
+  permutate: jest.fn()
 }));
 
-describe('Core', () => {
-  it('should call permutate util function with input', () => {
+describe("Core", () => {
+  it("should call permutate util function with input", () => {
     const input = [1, 2];
 
     safePermutate(input);
