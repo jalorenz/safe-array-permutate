e947350998e217adaf86058a369f02c8693ace15
Jannik Lorenz <janniklorenz1994@gmail.com>
2021-09-12 19:17:10 +0200

    feat(contracts): adds cut-off strategy and log level

--- lib/contracts.ts
+++ lib/contracts.ts
@@ -1,4 +1,17 @@
+export enum CutOffStrategy {
+  linear = 'linear',
+  random = 'random'
+}
+
+export enum CutOffLogLevel {
+  off = 'off',
+  warn = 'warn',
+  error = 'error'
+}
+
 export interface IPermutateOptions {
   returnDuplicates?: boolean;
   maxResultEntries?: number
+  cutOffStrategy?: CutOffStrategy
+  cutOffLogLevel?: CutOffLogLevel
 }
